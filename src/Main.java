d30923f22e7d958d5d8e7768ecf3ab5d8f48a3ad
fernando-daw <flagonovoa@danielcastelao.org>
2023-01-25 19:54:53 +0100

    # Conflicts: #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //A
         //B
+        // Lider A
+        //Lider B
+        //Lider C
+
         //Colaborador A
         //Colaborador B
         //Colaborador C
